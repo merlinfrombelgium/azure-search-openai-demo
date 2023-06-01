f03f55254b365d8706aacd5c173ac6452b3cdaef
Merlin from Belgium <merlinfrombelgium@gmail.com>
2023-06-01 17:49:47 +0200

    working demo

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Where do I start?",
+        value: "Where do I start?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What happens in a one-to-one?", value: "What happens in a one-to-one?" },
+    { text: "Who can I contact for more questions", value: "Who can I contact for more questions" }
 ];
 
 interface Props {
